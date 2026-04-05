182826aac8ab8bd4a45a90bdd5fca595498d1aa2
Fae <139820940+snuffbait@users.noreply.github.com>
2026-04-05 21:37:08 +0100

    Update dddd.py

--- dddd.py
+++ dddd.py
@@ -8,52 +8,32 @@ ms = 4800
 drop = 1.0
 throttle = 0.25
 ip = "ip"
-
 lag = False
-run = True
 packets = []
-
-def send():
-    while run:
-        now = time.time()
-        for i in range(len(packets) - 1, -1, -1):
-            if now >= packets[i][0]:
-                try:
-                    packets[i][1].send(packets[i][2])
-                except:
-                    pass
-                packets.pop(i)
-        time.sleep(0.01)
 
 def main():
     with pydivert.WinDivert(ip) as w:
-        while run:
-            try:
-                pkt = w.recv()
-            except:
-                break
-            
+        while True:
+            try: pkt = w.recv()
+            except: break
             if not lag:
-                w.send(pkt)
+                try: w.send(pkt)
+                except: pass
                 continue
-            
             if pkt.is_inbound:
-                if random.random() < drop:
-                    continue
-                packets.append((time.time() + ms / 1000, w, pkt))
-            else:
-                if random.random() <= throttle:
-                    w.send(pkt)
+                if random.random() >= drop: packets.append((time.time() + ms / 1000, w, pkt))
+            elif random.random() <= throttle:
+                try: w.send(pkt)
+                except: pass
 
-def click(x, y, button, pressed):
-    global lag
-    if pressed and button == mouse.Button.x1:
-        lag = not lag
+def send():
+    while True:
+        now = time.time()
+        packets[:] = [(t, w, p) for t, w, p in packets if now < t or not (lambda: w.send(p) or True)()]
+        time.sleep(0.01)
 
 threading.Thread(target=main, daemon=True).start()
 threading.Thread(target=send, daemon=True).start()
-mouse.Listener(on_click=click).start()
+mouse.Listener(on_click=lambda x, y, b, p: globals().update(lag=not lag) if p and b == mouse.Button.x1 else None).start()
 print("XButton1 = lag yo shit")
-while True:
-
-    time.sleep(1)
+while True: time.sleep(1)
